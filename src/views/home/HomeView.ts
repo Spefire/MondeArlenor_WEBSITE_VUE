1d15600df3588facd285d0684b33a99bc37f319f
Nicholas BRUN <nicholasb@hotmail.fr>
2022-01-03 22:27:53 +0100

    Feat(home): Add home structure and scroll

--- src/views/home/HomeView.ts
+++ src/views/home/HomeView.ts
@@ -1,17 +1,76 @@
 import { PageTitles } from "@/models/PagesTitles";
-import { ref } from "vue";
+import { defineComponent, Ref, ref } from "vue";
 
-export default {
+export default defineComponent({
   name: "HomeView",
   title: PageTitles.home,
   components: {},
 
   // eslint-disable-next-line
   setup() {
-    const title = ref("Hello my wooooorld");
+    const pages = ref(PageTitles);
+    const selection = ref(0);
+    const scrollValue = ref(0);
+    const timeSub: Ref<number | null> = ref(null);
 
     return {
-      title,
+      pages,
+      selection,
+      scrollValue,
+      timeSub
     };
   },
-};
+
+  mounted() {
+    window.addEventListener("mousewheel", this.mouseWheelEvent.bind(this));
+    window.addEventListener("DOMMouseScroll", this.mouseWheelEvent.bind(this));
+    
+    this.timeSub = setTimeout(()=>{
+      this.changeSelection();
+    }, 10000);
+  },
+
+  methods: {
+    increaseSelection(): void {
+      if (this.selection == 0) this.selection = 1;
+      else if (this.selection == 1) this.selection = 2;
+    },
+
+    changeSelection(): void {
+      if (this.selection == 0) this.selection = 2;
+      else if (this.selection == 1) this.selection = 0;
+      else if (this.selection == 2) this.selection = 1;
+    },
+
+    decreaseSelection(): void {
+      if (this.selection == 1) this.selection = 0;
+      else if (this.selection == 2) this.selection = 1;
+    },
+
+    setSelection(newSelection: number): void {
+      this.selection = newSelection;
+      this.launchTimeout();
+    },
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    mouseWheelEvent(e: any) {
+      this.scrollValue += e.wheelDelta ? e.wheelDelta : -e.detail;
+      if (this.scrollValue >= 360) {
+        this.scrollValue = -120;
+        this.decreaseSelection();
+        this.launchTimeout();
+      } else if (this.scrollValue <= -360) {
+        this.scrollValue = 120;
+        this.increaseSelection();
+        this.launchTimeout();
+      }
+    },
+
+    launchTimeout(): void {
+      if (this.timeSub) clearTimeout(this.timeSub);
+      this.timeSub = setTimeout(()=>{
+        this.changeSelection();
+      }, 10000);
+    }
+  }
+});
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,8 +1,63 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="../../assets/logo.png">
-  <p>{{ title }}</p>
+  <div class="home-view">
+    <div
+      :class="{'active': selection == 0 }"
+      class="home-container container01">
+      <div class="home-subcontainer">
+        <img
+          src="../../assets/images/logo.png"
+          alt="">
+        <h4>{{ pages.home }}</h4>
+        <h1>{{ pages.universe }}</h1>
+        <p>
+          Découvrez l'univers du Monde d'Arlénor, la religion dominante, la magie omni-présente, les différents peuples y vivant et les conflits y règnant...
+        </p>
+        <button routerLink="universe">Découvrir</button>
+      </div>
+    </div>
+    <div
+      :class="{'active': selection == 1 }"
+      class="home-container container02">
+      <div class="home-subcontainer inversed-container">
+        <h4>{{ pages.home }}</h4>
+        <h1>{{ pages.roleplay }}</h1>
+        <p>
+          Vivez une histoire épique à travers de jeunes héros, découvrant la notion du Destin et de l'effet papillon...
+        </p>
+        <button routerLink="roleplay">Osez l'aventure</button>
+      </div>
+    </div>
+    <div
+      :class="{'active': selection == 2 }"
+      class="home-container container03">
+      <div class="home-subcontainer">
+        <h4>{{ pages.home }}</h4>
+        <h1>{{ pages.celestia }}</h1>
+        <p>
+          Chaque personnage a une histoire. Découvrez celles de Romain, Elisa, Jérémy... et de tous les autres, sur l'île de Célestia.
+        </p>
+        <button>Prochainement disponible</button>
+      </div>
+    </div>
+    <ul class="selection-container">
+      <div class="dotline" />
+      <li
+        :class="{'active': selection == 0 }"
+        class="dot"
+        @click="setSelection(0)" />
+      <div class="dotline" />
+      <li
+        :class="{'active': selection == 1 }"
+        class="dot"
+        @click="setSelection(1)" />
+      <div class="dotline" />
+      <li
+        :class="{'active': selection == 2 }"
+        class="dot"
+        @click="setSelection(2)" />
+      <div class="dotline" />
+    </ul>
+  </div>
 </template>
 
 <script lang="ts" src="./HomeView.ts"></script>
