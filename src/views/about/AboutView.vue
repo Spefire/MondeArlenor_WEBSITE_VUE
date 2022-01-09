f7944c721a3a1a6686dcab019dc4036a1cdba3e9
Nicholas BRUN <nicholasb@hotmail.fr>
2022-01-10 00:08:51 +0100

    Feat(about): Add about view

--- src/views/about/AboutView.vue
+++ src/views/about/AboutView.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>AboutView</div>
+  <div class="about-view">
+    <div class="about-left">
+      <img
+        src="./../../assets/images/about.png"
+        alt="">
+    </div>
+    <div class="about-right">
+      <h1>A propos</h1>
+      <div separator />
+      <h2>Présentation</h2>
+      <p>
+        Je m'appelle Nicholas, aka Spefire, j'ai 27 ans
+        et je suis un ingénieur en informatique ayant une passion pour l'illustration et tout ce qui est graphique (ironique hein ?).
+        Je m'entraine sérieusement depuis 2015 en jongleant avec les fanarts, les jeux vidéo et le jeu de rôles...
+      </p>
+      <h3>Contact / Questions</h3>
+      <p>Vous pouvez me contacter sur <a href="mailto:spefire@gmail.com">spefire@gmail.com</a></p>
+      <h2>Projets</h2>
+      <h3>Le Monde d'Arlénor</h3>
+      <p>
+        Ce projet est né avec ma découverte du jeu de rôles en 2014.
+        J'avais envie de contrôler le monde dans lequel vivrait mes joueurs et créer mes propres scénarios très rapidement.
+        En janvier 2015, le Monde d'Arlénor est né. Mes premiers joueurs ont vécu une première campagne de 8 scénarios. 
+        Seulement, je souhaitais partager cet univers au delà du jeu de rôles... d'où ce site !
+      </p>
+      <h3>Livret "Etincelles"</h3>
+      <p>
+        Célestia est une sorte d'extension du Monde d'Arlénor : une île céleste encore inexplorée par les joueurs.
+        Je l'ai créé pour y faire vivre mes OCs (mes propres personnages) et partager leurs histoires.
+        Le livret a pour but de partager ces dernières.
+      </p>
+      <h2>Remerciements</h2>
+      <p>
+        Merci à mes joueurs Laetitia, Jérôme, Jonathan et Sylvain qui m'ont accompagné lors de la création d'Arlénor.
+        Merci à mes MJs d'autres univers qui m'ont énormément apporté en terme de conseils et de soutien.
+        Et puis merci à ce qui m'ont soutenu durant ce projet et ce site : Estelle, Alice, Audrey et mes amis testeurs...
+        et puis merci à toi de t'intéresser au Monde d'Arlénor !
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" src="./AboutView.ts"></script>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -7,7 +7,7 @@
         <img
           src="../../assets/images/logo.png"
           alt="">
-        <h2>{{ pages.home }}</h2>
+        <h3>{{ pages.home }}</h3>
         <h1>{{ pages.universe }}</h1>
         <p>
           Découvrez l'univers du Monde d'Arlénor, la religion dominante, la magie omni-présente, les différents peuples y vivant et les conflits y règnant...
@@ -23,7 +23,7 @@
       :class="{'active': selection == 1 }"
       class="home-container container02">
       <div class="home-subcontainer inversed-container">
-        <h2>{{ pages.home }}</h2>
+        <h3>{{ pages.home }}</h3>
         <h1>{{ pages.roleplay }}</h1>
         <p>
           Vivez une histoire épique à travers de jeunes héros, découvrant la notion du Destin et de l'effet papillon...
@@ -39,7 +39,7 @@
       :class="{'active': selection == 2 }"
       class="home-container container03">
       <div class="home-subcontainer">
-        <h2>{{ pages.home }}</h2>
+        <h3>{{ pages.home }}</h3>
         <h1>{{ pages.celestia }}</h1>
         <p>
           Chaque personnage a une histoire. Découvrez celles de Romain, Elisa, Jérémy... et de tous les autres, sur l'île de Célestia.
