ba41a71e6400d825f939cea868faa1f44fba5e8c
Nicholas BRUN <nicholasb@hotmail.fr>
2022-01-24 01:22:34 +0100

    Style(roleplay): Fix link buttons

--- src/views/roleplay/RoleplayView.vue
+++ src/views/roleplay/RoleplayView.vue
@@ -6,11 +6,13 @@
         Avant de créer son personnage, ou même de lire les règles du jeu de rôles...
         découvrez l'univers du Monde d'Arlénor : la religion dominante, la magie omni-présente, les différents peuples y vivant et les conflits y règnant...
       </p>
-      <router-link
-        class="link-button"
-        to="/universe">
-        Voir le Monde
-      </router-link>
+      <div>
+        <router-link
+          class="link-button"
+          to="/universe">
+          Voir le Monde
+        </router-link>
+      </div>
     </div>
     <div class="glyphe-container">
       <Glyphe :icon="'map'" />
@@ -29,16 +31,18 @@
         Créez votre personnage, des caractéristiques aux compétences, en passant par leurs pouvoirs...
         Accédez dès maintenant à la naissance de votre personnage !
       </p>
-      <router-link
-        class="link-button"
-        to="/roleplay/creation">
-        Passer à la création !
-      </router-link>
-      <router-link
-        class="margin-top-1 link-button"
-        to="/roleplay/docs">
-        Voir la documentation
-      </router-link>
+      <div>
+        <router-link
+          class="link-button"
+          to="/roleplay/creation">
+          Passer à la création !
+        </router-link>
+        <router-link
+          class="margin-top-1 link-button"
+          to="/roleplay/docs">
+          Voir la documentation
+        </router-link>
+      </div>
     </div>
   </div>
   <div little-separator />
@@ -48,21 +52,25 @@
       <p>
         Trouvez ici tous les éléments permettant de comprendre la création de personnage en profondeur ainsi que le système de jeu et des propositions de pouvoirs.
       </p>
-      <a
-        class="link-button margin-bottom-1"
-        href="./assets/files/Manuel_Joueur.pdf"
-        download>
-        Télécharger le Manuel du Joueur V2.1 (Pas à jour)
-      </a>
+      <div>
+        <a
+          class="link-button margin-bottom-1"
+          href="./assets/files/Manuel_Joueur.pdf"
+          download>
+          Télécharger le Manuel du Joueur V2.1 (Pas à jour)
+        </a>
+      </div>
       <p>
         Pour les Maitres du Jeu seulement : un guide sur la campagne et le premier scénario d'introduction.
       </p>
-      <a
-        class="link-button"
-        href="./assets/files/Manuel_MaitreDuJeu.pdf"
-        download>
-        Télécharger le Manuel du MJ V2.1 (Pas à jour)
-      </a>
+      <div>
+        <a
+          class="link-button"
+          href="./assets/files/Manuel_MaitreDuJeu.pdf"
+          download>
+          Télécharger le Manuel du MJ V2.1 (Pas à jour)
+        </a>
+      </div>
     </div>
     <div class="glyphe-container">
       <Glyphe :icon="'book'" />
