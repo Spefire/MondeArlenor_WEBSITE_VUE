7977c1fd42c7fb51be3f7d5744e122702511deb6
Nicholas BRUN <nicholasb@hotmail.fr>
2022-01-29 14:57:41 +0100

    Feat(crystals): Add previous view

--- src/views/universe/CrystalsView.vue
+++ src/views/universe/CrystalsView.vue
@@ -14,8 +14,46 @@
 
   <div little-separator />
 
-  <div class="population-view layout-view background universe-vertical">
-    <div class="layout-center large" />
+  <div class="crystals-view layout-view background universe-vertical">
+    <div class="layout-center large crystals-container">
+      <div class="crystals-zone-imgs">
+        <div class="crystal-imgs">
+          <img
+            class="crystal-main"
+            src="./../../assets/images/crystals/crystal.png"
+            alt="">
+          <img
+            class="crystal-data01"
+            src="./../../assets/images/crystals/crystal_data01.png"
+            alt="">
+          <img
+            class="crystal-data02"
+            src="./../../assets/images/crystals/crystal_data02.png"
+            alt="">
+          <img
+            class="crystal-data03"
+            src="./../../assets/images/crystals/crystal_data03.png"
+            alt="">
+        </div>
+      </div>
+      <div class="crystals-zone-text">
+        <h3>Le cristal arlénien</h3>
+        <h2>Propriétés connues par tous</h2>
+        <p>
+          Ils mesurent entre 1 cm et 2 m.<br>
+          La couleur d'un cristal est liée à son effet, à son pouvoir.<br>
+          Un cristal est très solide, résistant.<br>
+          L'utilisateur se lie à un cristal, avec une limite de 3 cristaux de rang (B ou supérieur) à la fois.<br>
+          Le lien peut se briser, mais il ne pourra pas se reformer entièrement par la suite.<br>
+          Plus l'utilisateur utilise ses cristaux, plus le lien entre eux s'intensifie.<br>
+          Plus l'utilisateur est loin de son cristal, plus il lui est difficile de l'utiliser.<br>
+          Les cristaux viennent principalement des mines de la cité désertique Jirakan.<br>
+          Seuls les cristalliens (métier complexe) peuvent les retravailler en leur donnant une forme précise.<br>
+          Les cristaux ne peuvent pas être séparés en plusieurs parties. Cassés, ils perdent leur pouvoir.<br>
+          Il existerait des cristaux uniques, dits 'légendaires', aux puissances incommensurables..."
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
