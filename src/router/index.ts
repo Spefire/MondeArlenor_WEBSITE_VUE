e6b31088cac6bec830e904ecb1990e929f8d46b7
Nicholas BRUN <nicholasb@hotmail.fr>
2022-02-04 19:16:51 +0100

    Fix(router): Add scrollToTop and add meta title

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import { PageTitles } from "@/models/PagesTitles";
 import AboutView from "@/views/about/AboutView.vue";
 import CelestiaView from "@/views/celestia/CelestiaView.vue";
 import HomeView from "@/views/home/HomeView.vue";
@@ -12,26 +13,37 @@ import UniverseView from "@/views/universe/UniverseView.vue";
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
-  { path: "/", component: HomeView },
+  { path: "/", component: HomeView, meta: { title: PageTitles.home }},
 
-  { path: "/universe", component: UniverseView },
-  { path: "/universe/religion", component: ReligionView },
-  { path: "/universe/magic", component: MagicView },
-  { path: "/universe/races", component: RacesView },
+  { path: "/universe", component: UniverseView, meta: { title: PageTitles.worldmap }},
+  { path: "/universe/religion", component: ReligionView, meta: { title: PageTitles.religion }},
+  { path: "/universe/magic", component: MagicView, meta: { title: PageTitles.magic }},
+  { path: "/universe/races", component: RacesView, meta: { title: PageTitles.races }},
 
-  { path: "/roleplay", component: RoleplayView },
-  { path: "/roleplay/creation", component: CreationView },
-  { path: "/roleplay/docs", component: DocsView },
+  { path: "/roleplay", component: RoleplayView, meta: { title: PageTitles.roleplay }},
+  { path: "/roleplay/creation", component: CreationView, meta: { title: PageTitles.creation }},
+  { path: "/roleplay/docs", component: DocsView, meta: { title: PageTitles.documentation }},
 
-  { path: "/celestia", component: CelestiaView },
+  { path: "/celestia", component: CelestiaView, meta: { title: PageTitles.celestia }},
 
-  { path: "/legacy", component: LegacyView },
-  { path: "/about", component: AboutView },
+  { path: "/legacy", component: LegacyView, meta: { title: PageTitles.legacy }},
+  { path: "/about", component: AboutView, meta: { title: PageTitles.about }},
   { path: "/*", redirect: "/" },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  scrollBehavior(_to, _from, savedPosition): any {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (_to.hash) {
+      return { el: _to.hash, behavior: "smooth" };
+    } else {
+      window.scrollTo(0, 0);
+    }
+  },
   routes
 });
 
