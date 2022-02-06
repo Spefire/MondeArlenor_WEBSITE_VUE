9a9b596434bcf1599b352cc91fd642d08d69d322
Nicholas BRUN <nicholasb@hotmail.fr>
2022-02-07 00:23:48 +0100

    Revert "Fix(mobile): Fix viewport on mobile ? (test01)"
    This reverts commit 22eda8fcdda21d04e71b8ce3bbe05996f192f1b1.

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,7 @@
-import { defineComponent } from "vue";
-
 import Footer from "./components/footer/Footer.vue";
 import Header from "./components/header/Header.vue";
 
-export default defineComponent({
+export default {
   name: "App",
   components: {
     Header,
@@ -14,13 +12,4 @@ export default defineComponent({
   setup() {
     return {};
   },
-
-  mounted() {
-    // We listen to the resize event
-    window.addEventListener("resize", () => {
-      // We execute the same script as before
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    });
-  }
-});
+};
