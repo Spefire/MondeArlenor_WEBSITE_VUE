b2e5ab841a07a9a7a638c9600a4e768dd2560527
Nicholas BRUN <nicholasb@hotmail.fr>
2022-02-10 00:54:39 +0100

    Feat(celestia): Add images

--- src/views/celestia/CelestiaView.vue
+++ src/views/celestia/CelestiaView.vue
@@ -23,7 +23,7 @@
           <div class="world-icons">
             <img
               class="world-map bloc-text"
-              src="./../../assets/images/world/worldmap_climates.jpg"
+              src="./../../assets/images/celestia/perso03.jpg"
               alt="Carte des Climats">
           </div>
         </div>
@@ -59,5 +59,5 @@
   </div>
 </template>
 
-<style lang="scss" scoped src="./CelestiaView.scss"></style>
+<style lang="scss" src="./CelestiaView.scss"></style>
 <script lang="ts" src="./CelestiaView.ts"></script>
