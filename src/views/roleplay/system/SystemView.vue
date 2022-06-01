1046f4ef1af7b1f3645c3cc6a1ad3dcd2774c68d
Nicholas BRUN <nicholasb@hotmail.fr>
2022-06-02 00:18:26 +0200

    Feat(system): Add text for the tests and difficulties

--- src/views/roleplay/system/SystemView.vue
+++ src/views/roleplay/system/SystemView.vue
@@ -291,13 +291,8 @@
       <h2>Les règles du système</h2>
       <div class="bloc-text margin-top-1 text-justify">
         &emsp;
-        Un personnage est défini par plusieurs choses : <b>ses caractéristiques</b> (et valeurs dérivées), <b>ses compétences principales</b>,
-        <b>sa race</b> (parmi celles jouables), <b>son cristal évolutif</b> (compétences spéciales ou sorts), pour ensuite finir par quelques finitions.
-        <br>&emsp;
-        Les caractéristiques définissent ce qu’est le personnage, et les compétences ce qu’il sait.
-        Votre personnage sera plus ou moins fort, plus ou moins intelligent, plus ou moins adroit.
-        Et c’est vous qui choisirez si, au cours de sa vie, il a appris à se battre ou s'il a préféré se tourner vers l'érudition,
-        ou encore, vers quel type de magie il s'est tourné...
+        Le système de jeu est une partie importante du jeu de rôles, puisque c’est grâce à lui que vous savez si votre personnage,
+        au cours du jeu, réussit ou non les actions qu’il entreprend.
       </div>
       <div class="bloc-text system-nav margin-top-1">
         <span
@@ -322,25 +317,71 @@
       <template v-if="systemChoice === 1">
         <div class="bloc-text margin-top-1 text-justify">
           &emsp;
-          La santé d’un personnage détermine sa résistance aux coups et aux événements qui pourraient le blesser.
-          Elle est indiquée par ses <b>Points de Vie</b>, ou PV.<br>
+          Le <b>test classique</b> qui détermine si un personnage réussit ou non une action se réalise de la façon suivante :
+          le Maitre du Jeu <b>détermine quelle caractéristique de base du personnage est concernée</b> (Vigueur, Habileté, Intellect, Présence, Pouvoir)
+          ensuite le joueur lance autant de dés à 6 faces qu’il a de points dans la caractéristique,
+          additionne les résultats, puis rajoute à la fin tous les points bonus ou malus
+          (dûs aux compétences, aux équipements ou aux effets appliqués sur le personnage).
+          <br>&emsp;
+          Le MJ compare ensuite le total à la difficulté nécessaire pour la résolution du test.
+          Si le total est supérieur ou égal à cette difficulté, le joueur réussit son action.
           <br>
-          <div class="text-center">
-            <b>Indemne</b> (+ de 6 PV) : Aucun malus<br>
-            <b>Blessé</b> (2-5 PV) : - 1D6 aux jets<br>
-            <b>Au seuil de la mort</b> (1 PV) : - 2D6 aux jets<br>
+          <br>&emsp;
+          Note : Une <b>réussite critique</b>, c'est quand tous les dés lancés <b>affichent un 6</b>.
+          Le joueur peut alors <b>lancer un dé supplémentaire</b> afin d'améliorer son score.
+          <br>&emsp;
+          Note : Un <b>échec critique</b>, c'est quand tous les dés lancés <b>affichent un 1</b>.
+          Le joueur <b>rate alors automatiquement son test</b>, et le MJ peut décider d'y <b>ajouter une catastrophe</b> en plus de son échec.
+        </div>
+          
+        <div class="bloc-text margin-top-1 text-center">
+          <div class="margin-bottom-1">
+            <b>Action simple : Difficulté 6</b><br>
+            La majorité des personnages peut le faire rapidement, et un spécialiste n’éprouve aucune difficulté.<br>
+            <i>Exemple : Obtenir des informations sur une légende urbaine bien connue.</i>
           </div>
-          <br>
-          Note : Le nombre de PV max peut varier avec la Vigueur.<br>
-          <br>&emsp;
-          Lors de jets de combat ou de situations pouvant occasionner des blessures,
-          le Maitre du Jeu interprétera le nombre de PV à enlever.
-          Et lorsque les blessures s’accumulent, le personnage s’affaiblit petit à petit, ajoutant des malus à ses actions.
-          <br>&emsp;
-          Dans les cas extrêmes, certaines attaques n’ont pas besoin d’en passer par des tests quelconques.
-          Un personnage armé d’une épée qui veut tuer un adversaire ligoté y parviendra directement,
-          de même qu’un autre qui sera sous le point de chute d’un tir de catapulte n’a aucune chance de s’en tirer.
-          Il faut faire preuve de logique.
+          <div class="text-center margin-bottom-1">
+            <b>Action complexe : Difficulté 14</b><br>
+            Ce genre d’action pose de sérieux problèmes aux profanes, mais les personnes entraînées ou naturellement douées les réussissent rapidement.<br>
+            <i>Exemple : Conduire un attelage de nuit sur une route cahoteuse.</i>
+          </div>
+          <div class="text-center margin-bottom-1">
+            <b>Action difficile : Difficulté 20</b><br>
+            Quasi impossible sans formation à moins de circonstances favorables ou d’un don particulièrement marqué,
+            ces actions n’ont de bonne chance d’être réussies que par les spécialistes.<br>
+            <i>Exemple : Trouver de quoi se nourrir en hiver pendant plusieurs semaines dans un bois profond.</i>
+          </div>
+          <div class="text-center">
+            <b>Action épique : Difficulté 30 et plus</b><br>
+            Seule une poignée de personnes peuvent espérer réussir un tel défi, qui demande des aptitudes quasi surnaturelles.<br>
+            <i>Exemple : Au cours d’une réception, voler la couronne de la Reine, qu’elle porte sur la tête, au vu de tous et sans se faire remarquer.</i>
+          </div>
+        </div>
+
+        <div class="bloc-text margin-top-1 text-justify">
+          <div class="text-center"><i>Exemple n°1</i></div>
+          <b>Hélios</b> cherche dans une bibliothèque une information sur une plante inconnue.<br>
+          Si le livre peut donner l'information, le MJ demandera un test d'<b>Intellect</b>.<br>
+          Hélios a <b>3 en Intellect</b>, il lance <b>3D6</b> et fait... <b>4,5,2 pour un résultat de 11</b>.<br>
+          Hélios possède une <b>compétence "Rat de bibliothèque"</b> qui lui donne un <b>bonus +4</b> dans la réalisation de cette action.<br>
+          En dehors de ça, Hélios ne possède pas d'effet magique, ni d'équipement qui lui donnerait un autre bonus ou un malus pour cette action.<br>
+          <b>Le total est donc 11 + 4 = 15.</b><br>
+          Le MJ avait décidé que pour trouver cette information, c'était une <b>action complexe (difficulté à 14)</b>.<br>
+          <b>Hélios a réussi</b>, il trouve que la plante inconnue est un Hibicus moscheutos.
+        </div>
+
+        <div class="bloc-text margin-top-1 text-justify">
+          <div class="text-center"><i>Exemple n°2</i></div>
+          <b>Ophéliz</b> cherche à trancher les chaînes d'un prisonnier avec son épée.<br>
+          C'est une épée longue (donc lourde), le MJ demandera un test de <b>Vigueur</b>.<br>
+          Ophéliz a <b>2 en Vigueur</b>, elle lance <b>2D6</b> et fait... <b>6 et 6</b> ! C'est une <b>réussite critique</b> !
+          Elle a le droit de relancer un dé pour augmenter son score, et fait... <b>5, pour un résultat de 17</b>.<br>
+          Ophéliz possède une <b>compétence "Armes à une main (épées)"</b> qui lui donne un <b>bonus +4</b> dans la réalisation de cette action.<br>
+          Elle possède en plus un <b>effet magique</b> sur son arme (qui donne un <b>bonus +2</b> sans condition).<br>
+          Et malheureusement pour Ophéliz, dans la prison, il fait nuit et il n'y a pas d'éclairage : le MJ décide alors qu'elle a un <b>malus -4</b> pour cette action.<br>
+          <b>Le total est donc 17 + 4 + 2 - 4 = 19.</b><br>
+          Le MJ avait décidé que pour trancher cette chaîne d'un seul coup, c'était une <b>action épique (difficulté à 30)</b>.<br>
+          <b>Ophéliz n'a pas réussi</b>, mais la chaîne s'est un peu abîmée. Le MJ peut lui dire qu'à force la chaîne cédera peut-être...
         </div>
       </template>
       <template v-if="systemChoice === 2">
