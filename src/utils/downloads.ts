b6c02617e277b3e46e48e67949ea602074a48d6c
Nicholas BRUN <nicholasb@hotmail.fr>
2022-09-18 18:43:14 +0200

    Feat(creation): Add second page with powers

--- src/utils/downloads.ts
+++ src/utils/downloads.ts
@@ -6,8 +6,8 @@ const downloadPDF = async(character: ArlenorCharacter): Promise<void> => {
   const width = doc.internal.pageSize.getWidth();
   const height = doc.internal.pageSize.getHeight();
 
-  const background = await require("../assets/files/Fiche_PersoVide.jpeg");
-  doc.addImage(background, "JPEG", 0, 0, width, height);
+  const backgroundRecto = await require("../assets/files/Fiche_PersoVide_Recto.jpeg");
+  doc.addImage(backgroundRecto, "JPEG", 0, 0, width, height);
   doc.setFontSize(10);
 
   // --- AVATAR ET IDENTITE
@@ -43,6 +43,20 @@ const downloadPDF = async(character: ArlenorCharacter): Promise<void> => {
   y = 134 + 21 * 4;
   doc.text("" + character.race?.name, x, y, { align: "center" });
 
+  // --- COMPETENCES RACIALES
+  const x_txt_scale = 38;
+  const y_txt_scale = 12.5;
+  const y_line_scale = 31.925;
+  const iconCapacity = await require("../assets/icons/capacities/capacity_60.png");
+
+  x = 243.4;
+  y += 19.45;
+  for(let i = 0; i < 4; i++) {
+    doc.addImage(iconCapacity, "JPEG", x, y, 20, 20);
+    doc.text("Nom de la compétence raciale", x + x_txt_scale, y + y_txt_scale);
+    y += y_line_scale;
+  }
+
   // --- PROFIL
   x = 28;
   y = 515.5;
@@ -61,6 +75,48 @@ const downloadPDF = async(character: ArlenorCharacter): Promise<void> => {
     align: "justify",
     maxWidth: 180,
   });
+
+  doc.addPage();
+  const backgroundVerso = await require("../assets/files/Fiche_PersoVide_Verso.jpeg");
+  doc.addImage(backgroundVerso, "JPEG", 0, 0, width, height);
+  doc.setFontSize(10);
+
+  // --- COMPETENCES
+  const iconSkill = await require("../assets/icons/skills/armes.png");
+
+  x = 34.6;
+  y = 35.8;
+  for(let i = 0; i < 5; i++) {
+    doc.addImage(iconSkill, "JPEG", x, y, 20, 20);
+    doc.text("Nom de la compétence", x + x_txt_scale - 1, y + y_txt_scale);
+    y += y_line_scale;
+  }
+
+  x = 243.4;
+  y = 35.8;
+  for(let i = 0; i < 5; i++) {
+    doc.addImage(iconSkill, "JPEG", x, y, 20, 20);
+    doc.text("Nom de la compétence", x + x_txt_scale, y + y_txt_scale);
+    y += y_line_scale;
+  }
+
+  // --- POUVOIRS
+  const iconPower = await require("../assets/icons/powers/pouvoir_mental.png");
+
+  x = 34.6;
+  y = 227;
+  for(let i = 0; i < 12; i++) {
+    doc.addImage(iconPower, "JPEG", x, y, 20, 20);
+    doc.text("Nom du pouvoir", x + x_txt_scale - 1, y + y_txt_scale);
+    doc.setFontSize(8);
+    doc.text("Description du pouvoir...description du pouvoir...description du pouvoir...description du pouvoir..."
+    +"description du pouvoir...description du pouvoir...description du pouvoir...description du pouvoir...", x + 202, y + y_txt_scale - 2, {
+      align: "justify",
+      maxWidth: 180,
+    });
+    doc.setFontSize(10);
+    y += y_line_scale;
+  }
 
   doc.save("Arlenor_" + character.name + ".pdf");
 };
