3405a576a33482281146275eff027dd1e027fb2a
Nicholas BRUN <nicholasb@hotmail.fr>
2023-09-09 00:21:11 +0200

    Fix(faction): Remove import unused

--- src/views/universe/factions/FactionsView.ts
+++ src/views/universe/factions/FactionsView.ts
@@ -1,5 +1,4 @@
 import HeadLayout from "@/components/head-layout/HeadLayout.vue";
-import InfoLayout from "@/components/info-layout/InfoLayout.vue";
 import { ArlenorFaction } from "@/models/ArlenorFaction";
 import { getListFactions } from "@/models/data/ListFactions";
 import { PageTitles } from "@/models/PagesTitles";
@@ -10,7 +9,6 @@ export default defineComponent({
   title: PageTitles.factions,
   components: {
     HeadLayout,
-    InfoLayout,
   },
 
   setup() {
